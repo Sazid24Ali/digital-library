3828d361106f2e248ac84cfc046f19dbc4471716
Sazid24Ali <shaikmohammedsazidali@gmail.com>
2025-03-28 05:54:40 +0530

    generated jar file

--- src/test/java/com/digital_lib/inter/InterApplicationTests.java
+++ src/test/java/com/digital_lib/inter/InterApplicationTests.java
@@ -3,7 +3,9 @@ package com.digital_lib.inter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.digital_lib.intern.InterApplication;
+
+@SpringBootTest(classes = InterApplication.class)
 class InterApplicationTests {
 
 	@Test
